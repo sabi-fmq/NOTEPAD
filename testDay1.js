8f324230a27b320a6c98c271ed0cd3d8377f990e
Sabi <sabialam687@gmail.com>
2022-08-23 15:21:10 +0530

    First commit

--- testDay1.js
+++ testDay1.js
@@ -1,3 +1,18 @@
-console.log("sabi alam ansari");
-console.log(3+5);
-console.log("Hello Rahul")
+console.log("for loop start here");
+const p = 4;
+for (let i =1; i<=p ; i++){
+    console.log("your i is = ", i);
+}
+console.log("do while loop start here");
+let j =1;
+do{
+    j++;
+    console.log("your j is = ", j);
+}while(j<=4);
+console.log("while loop start here");
+let n= 1;
+while(n<=4){
+    console.log("your n is = ", n);
+     n++;
+    // console.log("your n is = ", n);
+}
---
+++ testday2.js
@@ -0,0 +1,75 @@
+// let x = 0;
+// let z = 0;
+// labelCancelLoops: while (true) {
+//   console.log('Outer loops: ', x);
+//   x += 1;
+//   z = 1;
+//   while (true) {
+//     console.log('Inner loops: ', z);
+//     z += 1;
+//     if (z === 10 && x === 10) {
+//       break labelCancelLoops;
+//     } else if (z === 10) {
+//       break;
+//     }
+//   }
+// }
+// console.log('continue example');
+// let i = 0;
+// let n = 0;
+// while (i < 5) {
+//   i++;
+//   if (i === 3) {
+//     continue;
+//   }
+//   n += i;
+//   console.log(n);
+// }
+// let i = 0;
+// let j = 10;
+// checkiandj: while (i < 4) {
+//   console.log(i);
+//   i += 1;
+//   checkj: while (j > 4) {
+//     console.log(j);
+//     j -= 1;
+//     if ((j % 2) === 0) {
+//       continue checkj;
+//     }
+//     console.log(j, ' is odd.');
+//   }
+//   console.log('i = ', i);
+//   console.log('j = ', j);
+// }
+// let obj1 = ['a','s','d','f','g','h','j','k','l'];
+// console.log(obj1);
+// for (let i in obj1) {
+  
+//     // Prints all the keys in
+//     // obj1 on the console
+//     console.log(i);
+// } 
+const myString = "My Nmae is Sabi Alam" ;
+const obj1 = {
+    FirstName : 'Sabi',
+    company : "FMQ",
+     experience : {
+        start : 'aug',
+        end : 'Nov'
+    }
+}
+let temp = ''; 
+// for(let key of obj1){
+    
+//     console.log(obj1.key ,":",obj1[key]);
+// }
+console.log("in loop");
+for(let key in obj1){
+   if( typeof key === 'object' &&
+   !Array.isArray(key) &&
+   key !== null)
+    console.log(key ,":",obj1.key[key]);
+    else(
+        console.log(key,":",obj1[key])
+    )
+}
